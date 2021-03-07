45a6818321a9099cef55fe33fef5eaa7549a0abb
root <root@DELL7559.localdomain>
2021-03-07 21:44:11 +0800

    update

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,29 +1,8 @@
-#!/usr/bin/env bash
-
-git fetch
-git reset --hard origin/master
+#!/bin/sh
+set -e
+echo "git 拉取 ASMShell 最新代码..."
+cd ${ASMShell_DIR}/docker
+git reset --hard
 git pull
-
-echo "设定远程仓库地址..."
-mkdir -p /root/.ssh \
-    && chmod 700 /root/.ssh \
-    && cd /root/.ssh \
-    && echo -e $KEY > /root/.ssh/id_rsa \
-    && chmod 600 /root/.ssh/id_rsa \
-    && ssh-keyscan github.com > /root/.ssh/known_hosts
-[ ! -d ${ASMDIR}/.git ] && echo "git clone克隆最新代码..." && git clone -b ${ASMBRANCH} ${ASMURL} ${ASMDIR}
-cd  ${ASMDIR} && echo "git pull最新代码..." && git pull
-echo "npm install 安装最新依赖"
-npm config set registry https://registry.npm.taobao.org 
-npm install -s --prefix ${ASMDIR} >/dev/null
-echo "------------------------------------------------------------------------------------------------"
-echo "生成任务列表..."
-cat ${ASMDIR}/commands/tasks/unicom/unicom.js | sed '/\/\*\*\*/,/\*\*\*\//d' | sed '/\/\*/,/\*\//d'|sed '/\/\//d' | grep -oE "\"[a-z A-Z0-9]+\""| cut -f2 -d\">${ASMShellDIR}/config/task.list
-echo "已指定计划任务配置${crontab_file}，将直接使用该文件"
-/usr/bin/crontab ${crontab_file}
-echo "复制配置文件..."
-cp -f ${ASMShellDIR}/config/${envfile} ${ASMDIR}/config/.env
-echo "程序启动完毕..."
-/usr/sbin/crond -S -c /var/spool/cron/crontabs -f -L /dev/stdout
-crond
-echo "------------------------------------------------------------------------------------------------"
+echo "启动startup..."
+bash start startup
---
+++ docker/start.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+if [ $1 ]; then
+  apk --no-cache add -f coreutils moreutils nodejs npm perl openssl openssh-client libav-tools libjpeg-turbo-dev libpng-dev libtool libgomp tesseract-ocr graphicsmagick
+  npm config set registry https://registry.npm.taobao.org
+  echo "配置仓库更新密钥..."
+  mkdir -p /root/.ssh
+  echo -e ${KEY} >/root/.ssh/id_rsa
+  chmod 600 /root/.ssh/id_rsa
+  ssh-keyscan github.com >/root/.ssh/known_hosts
+  echo "容器启动，拉取脚本仓库代码..."
+  if [ -f "${ASMShell_DIR}/scripts/AutoSignMachine.js" ]; then
+    echo "仓库已经存在，跳过clone操作..."
+  else
+    if [  -d ${ASMShell_DIR}/tmp ];then
+      rmdir ${ASMShell_DIR}/tmp
+    fi
+    git clone --no-checkout -b ${ASMShell_BRANCH} ${REPO_URL} ${ASMShell_DIR}/tmp
+    mv ${ASMShell_DIR}/tmp/.git ${ASMShell_DIR}/scripts
+    rmdir ${ASMShell_DIR}/tmp
+  fi
+fi
+
+echo "git pull拉取最新代码..."
+cd ${ASMShell_DIR}/scripts
+git reset --hard HEAD
+git fetch --all
+git reset --hard origin/${ASMShell_BRANCH}
+
+echo "npm install 安装最新依赖"
+npm install -s --prefix ${ASMShell_DIR}/scripts >/dev/null
+function buildcron {
+  JS_file=${Scripts_DIR}/commands/tasks/unicom/unicom.js
+  Taskarray=(`cat ${JS_file} | sed '/\/\*\*\*/,/\*\*\*\//d' | sed '/\/\*/,/\*\//d'|sed '/\/\//d' | grep -oE "\"[a-z A-Z0-9]+\""| cut -f2 -d\"`)
+  for ((i=0;i<${#Taskarray[@]};i++))
+  do
+    Line=$(sed -n "/\"${Taskarray[$i]}\"/=" ${JS_file})
+    printf "#####($(($i+1)))"
+    cat ${JS_file} | sed -n $(($Line-3)),$(($Line-2))p | grep \/\/ | sed 's/[ \t]*//g'|sed 's/[\/\/\t]*//g'|sed 's/^/#&/g'
+    if [ $i>12 ];then 
+      min=$(($i*5-$i/12*60))
+      hour=$((8+$i/12))
+    else 
+      min=$(($i*5))
+      hour=8
+    fi
+    echo "$min $hour * * * bash u ${Taskarray[$i]}"
+  done
+}
+echo "------------------------------------------------------------------------------------------------"
+echo "生成cron列表..."
+buildcron>${ASMShell_DIR}/config/crontab.sh
+echo "\n0 18 * * *  node ${Scripts_DIR}/index.js unicom --tryrun --tasks  $(echo ${Taskarray[@]}|tr "\ " ",") |ts>> /ASMShell/logs/all.txt 2>&1 &">>${ASMShell_DIR}/config/crontab.sh
+echo "\n0 */4 * * * bash start">>${ASMShell_DIR}/config/crontab.sh
+echo "\n0 0 */3 * * rm -rf ${ASMShell_DIR}/logs/*.log">>${ASMShell_DIR}/config/crontab.sh
+echo "指定cron配置${crontab_file}"
+/usr/bin/crontab ${ASMShell_DIR}/config/crontab.sh
+echo "复制${env_file}配置至.env"
+cp -f ${ASMShell_DIR}/config/${env_file} ${Scripts_DIR}/config/.env
+echo "程序启动完毕..."
+/usr/sbin/crond -S -c /var/spool/cron/crontabs -f -L /dev/stdout
+
+echo "------------------------------------------------------------------------------------------------"
