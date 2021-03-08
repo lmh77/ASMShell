1e41b3a1ce8f4ac27a42c2b58d82f180a4372ea0
root <root@DELL7559.localdomain>
2021-03-08 22:33:34 +0800

    update

--- docker/start.sh
+++ docker/start.sh
@@ -8,6 +8,9 @@ if [ $1 ]; then
   echo -e ${KEY} >/root/.ssh/id_rsa
   chmod 600 /root/.ssh/id_rsa
   ssh-keyscan github.com >/root/.ssh/known_hosts
+  git config --global pull.ff only
+  echo "13.250.177.223 github">>/etc/hosts
+  cat /etc/hosts | sort -u>/etc/hosts
   echo "容器启动，拉取脚本仓库代码..."
   if [ -f "${ASMShell_DIR}/scripts/AutoSignMachine.js" ]; then
     echo "仓库已经存在，跳过clone操作..."
@@ -17,7 +20,7 @@ if [ $1 ]; then
     npm install -s --prefix ${Scripts_DIR} >/dev/null
   fi
 fi
-git config --global pull.ff only
+
 echo "git pull拉取最新代码..."
 cd ${Scripts_DIR}
 git pull
