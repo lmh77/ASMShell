f98545d182b3ef82e75cc1bd65556285375777c1
lmh77 <62322755+lmh77@users.noreply.github.com>
2021-03-09 00:37:00 +0800

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 set -e
 echo "git 拉取 ASMShell 最新代码..."
-cd ${ASMShell_DIR}/docker
-git reset --hard
-git pull
+cd ${ASMShell_DIR}/docker && git reset --hard && git pull
 echo "启动startup..."
 bash start startup
 /usr/sbin/crond -S -c /var/spool/cron/crontabs -f -L /dev/stdout
