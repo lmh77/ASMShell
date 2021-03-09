72458430f63396f862866ef3fb581a9d2e6d610e
root <root@DELL7559.localdomain>
2021-03-09 11:09:00 +0800

    update

--- docker/buildcron.sh
+++ docker/buildcron.sh
@@ -17,15 +17,32 @@ function build {
   done
 }
 echo "生成crontab.sh.sample文件..."
+rm -rf ${ASMShell_DIR}/config/crontab.sh.sample
 build>${ASMShell_DIR}/config/crontab.sh.sample
-echo "0 15 * * *  bash <(bash all)">>${ASMShell_DIR}/config/crontab.sh
+echo "0 15 * * *  bash <(bash all)">>${ASMShell_DIR}/config/crontab.sh.sample
 echo "0 */4 * * * bash start">>${ASMShell_DIR}/config/crontab.sh.sample
 echo "0 0 */3 * * rm -rf ${ASMShell_DIR}/logs/*.log">>${ASMShell_DIR}/config/crontab.sh.sample
-echo "指定cron配置crontab.sh"
-/usr/bin/crontab ${ASMShell_DIR}/config/crontab.sh
 
 #判断......
 function decide {
-  cp -f ${ASMShell_DIR}/config/crontab.sh.sample ${ASMShell_DIR}/config/crontab.sh
-
+  if [ -f "${ASMShell_DIR}/config/crontab.sh" ]; then
+    echo "仓库已经存在crontab.sh..."
+    diff ${ASMShell_DIR}/config/crontab.sh ${ASMShell_DIR}/config/crontab.sh.sample > /dev/null
+    if [ $0 == 0 ]; then
+        echo "crontab配置文件一致..."
+    else
+        echo "crontab配置文件不一致..."
+        echo "备份原配置..."
+        cp -f ${ASMShell_DIR}/config/crontab.sh ${ASMShell_DIR}/config/crontab.sh.bak
+        echo "生成新配置..."
+        cp -f ${ASMShell_DIR}/config/crontab.sh.sample ${ASMShell_DIR}/config/crontab.sh
+    fi
+  else
+    echo "仓库没有crontab.sh..."
+    echo "复制sample生成crontab.sh..."
+    cp -f ${ASMShell_DIR}/config/crontab.sh.sample ${ASMShell_DIR}/config/crontab.sh
+  fi
 }
+decide
+echo "指定cron配置为crontab.sh"
+/usr/bin/crontab ${ASMShell_DIR}/config/crontab.sh
--- docker/start.sh
+++ docker/start.sh
@@ -3,9 +3,10 @@ if [ $1 ]; then
   #仅第一次启动拉取代码和依赖
   #后续新增则手动执行相应命令
   node -v>/dev/null 2>&1
-  if [[ $? -ne 0 ]]; then                                 
-    apk update
-    apk --no-cache add -f coreutils moreutils nodejs npm perl openssl openssh-client libav-tools libjpeg-turbo-dev libpng-dev libtool libgomp tesseract-ocr graphicsmagick >/dev/null 2>&1
+  if [[ $? -ne 0 ]]; then
+    echo "第一次启动容器..."
+    echo "下载软件包......."
+    apk update && apk --no-cache add -f coreutils moreutils nodejs npm perl openssl openssh-client libav-tools libjpeg-turbo-dev libpng-dev libtool libgomp tesseract-ocr graphicsmagick >/dev/null 2>&1
     git config --global pull.ff only
     npm config set registry https://registry.npm.taobao.org
     echo "配置仓库更新密钥..."
@@ -15,18 +16,20 @@ if [ $1 ]; then
     ssh-keyscan github.com >/root/.ssh/known_hosts
     echo "拉取脚本仓库代码..."
     git clone -b ${Scripts_BRANCH} ${Scripts_URL} ${Scripts_DIR}
-    echo "npm install安装依赖"
-    npm install -s --prefix ${Scripts_DIR} >/dev/null
+    echo "npm安装依赖........"
+    npm install -s --prefix ${Scripts_DIR} >/dev/null 2>&1
   fi
 fi
-
-echo "< 2>------------------------------------------------------------------------------------------------"
-echo "git pull拉取ASM更新..."
+cat /etc/hosts | grep github || echo "13.250.177.223 github" >>/etc/hosts && echo "52.74.223.119 github" >>/etc/hosts
+echo "<2>------------------------------------------------------------------------------------------------"
+echo "更新ASM......"
 cd ${Scripts_DIR} && git pull
 
 #后续补充需求
 ln -sf ${ASMShell_DIR}/docker/all.sh /usr/local/bin/all
 ln -sf ${ASMShell_DIR}/docker/buildcron.sh /usr/local/bin/buildcron
+ln -sf ${ASMShell_DIR}/docker/u.sh /usr/local/bin/u
+ln -sf ${ASMShell_DIR}/docker/start.sh /usr/local/bin/start
 
 
 bash buildcron
@@ -34,5 +37,6 @@ bash buildcron
 #多账号并发指定配置文件
 echo "复制${env_file}配置至.env"
 cp -f ${ASMShell_DIR}/config/${env_file} ${Scripts_DIR}/config/.env
-echo "更新代码及配置完毕..."
+echo "代码及配置更新完毕..."
+echo "启动完毕..."
 echo "<3>------------------------------------------------------------------------------------------------"
