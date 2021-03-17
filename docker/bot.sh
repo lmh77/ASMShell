4946d7158645d0f662622aca37cf9be361f7ceee
lmh77 <62322755+lmh77@users.noreply.github.com>
2021-03-17 17:25:00 +0800

    Update bot.sh

--- docker/bot.sh
+++ docker/bot.sh
@@ -1,7 +1,4 @@
 if [ hostname==asm1 ];then
-  ls -l ${Scripts_DIR}/TGShell/index.js>/dev/null 2>&1
-  if [[ $? -ne 0 ]]; then
-    cd ${ASMShell_DIR} && git clone https://github.com/lmh77/TeleShellBot.git TGShell
-  fi
+  [ ! -d ${Scripts_DIR}/TGShell ] && cd ${ASMShell_DIR} && git clone https://github.com/lmh77/TeleShellBot.git TGShell
   cd ${ASMShell_DIR}/TGShell && npm install && nohup node index.js >${ASMShell_DIR}/logs/.logs 2>&1 &
 fi   
