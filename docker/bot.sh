afa027e285513680870f5faaa65d05f3c067bb30
lmh77 <62322755+lmh77@users.noreply.github.com>
2021-03-17 17:29:26 +0800

    Delete bot.sh

--- docker/bot.sh
+++
@@ -1,4 +0,0 @@
-if [ hostname==asm1 ];then
-  [ ! -d ${Scripts_DIR}/TGShell ] && cd ${ASMShell_DIR} && git clone https://github.com/lmh77/TeleShellBot.git TGShell
-  cd ${ASMShell_DIR}/TGShell && npm install && nohup node index.js >${ASMShell_DIR}/logs/.logs 2>&1 &
-fi   
